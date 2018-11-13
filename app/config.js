26e901028498f74af4197b61b23832e8907f11d5
srich.leeds@gmail.com <srich.leeds@gmail.com>
2018-11-13 20:40:49 +0000

    Fixing service name in banner

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Apply for Newstyle Employment and Support Allowance',
+  serviceName: 'Apply for new style Employment and Support Allowance',
 
   // Default port that prototype runs on
   port: '3000',
