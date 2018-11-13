324441efd06c30ad91ad9da676d0ccf6d1dfc05e
srich.leeds@gmail.com <srich.leeds@gmail.com>
2018-11-13 09:11:08 +0000

    Updating service name in black banner to Apply for Newstyle Employment and Support Allowance

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Apply for Employment and Support Allowance',
+  serviceName: 'Apply for Newstyle Employment and Support Allowance',
 
   // Default port that prototype runs on
   port: '3000',
