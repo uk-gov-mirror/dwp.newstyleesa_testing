0b8ecfec06444b97fbc5c5dfd219af909dc038e6
philswan <mail@philswan.co.uk>
2019-03-07 15:39:31 +0000

    Add conditional stuff to fit for work screen - hacky but works

--- app/routes.js
+++ app/routes.js
@@ -52,6 +52,45 @@ router.get('/*/claimdate', function (req, res, next) {
   next();
 })
 
+// Adding the moment plug in for the fit for work screen
+router.get('/*/fit-for-work', function (req, res, next) {
+  var ssp = req.session.data['ssp-dob-year'] + '-' +req.session.data['ssp-dob-month'] + '-' + req.session.data['ssp-dob-day'];
+  ssp = moment(ssp, 'YYYY-MM-DD');
+  
+  var recent = req.session.data['ssp-recent-dob-year'] + '-' +req.session.data['ssp-recent-dob-month'] + '-' + req.session.data['ssp-recent-dob-day'];
+  recent = moment(recent, 'YYYY-MM-DD');
+  
+  var last = req.session.data['last-dob-year'] + '-' +req.session.data['last-dob-month'] + '-' + req.session.data['last-dob-day'];
+  last = moment(last, 'YYYY-MM-DD');
+  
+  var dateToShow, whichDate;
+  if (last.isValid() && req.session.data['self-employed'] == 'mines') {
+    whichDate = 'lastWorked';
+    dateToShow = last;
+  }
+
+  if(recent.isValid()) {
+    whichDate = 'sspRecent';
+    dateToShow = recent;
+  }
+
+  if(ssp.isValid()) {
+    whichDate = 'ssp';
+    dateToShow = ssp;
+  }
+
+  if(dateToShow > moment().subtract(3, 'months')) {
+    dateToShow.add(1, 'days');
+    res.locals.dateToShow = dateToShow.format('D MMMM YYYY');
+    res.locals.whichDate = whichDate;
+  } else {
+    res.locals.whichDate = whichDate;
+    res.locals.date3monthsAgo = moment().subtract(3, 'months').add(1, 'days').format('D MMMM YYYY')
+  }
+
+  next();
+})
+
 // Page routing
 router.get('*', function (req, res, next) {
   if (req.params[0].substr(-1) == '/') res.locals.path = req.params[0].slice(0,-1).substr(1);
