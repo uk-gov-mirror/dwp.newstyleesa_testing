cb4426b976ec9d92027c11136d7f4c15952927b8
Phil Swan <mail@philswan.co.uk>
2019-04-26 12:04:55 +0100

    Add dynamic sesion labels

--- app/routes.js
+++ app/routes.js
@@ -32,20 +32,41 @@ router.get('/apply/v9/list', (req, res, next) => {
   if (!req.session.sectionStatus){
     console.log('no session');
     req.session.sectionStatus = {
-      cwyn: 'complete',
-      yh: undefined,
+      // cwyn: 'complete',
+      yourhealth: undefined,
       paidwork: undefined,
-      sp: undefined,
-      vw: undefined,
-      pensions: undefined,
-      ipi: undefined,
-      yd: undefined,
+      ssp: undefined,
+      voluntarywork: undefined,
+      pension: undefined,
+      insurance: undefined,
+      yourdetails: undefined,
       submitted: undefined,
     }
   }
 
+  if (req.query.yourhealth) {
+    req.session.sectionStatus.yourhealth = req.query.yourhealth
+  };
   if (req.query.paidwork) {
     req.session.sectionStatus.paidwork = req.query.paidwork
+  };
+  if (req.query.ssp) {
+    req.session.sectionStatus.ssp = req.query.ssp
+  };
+  if (req.query.voluntarywork) {
+    req.session.sectionStatus.voluntarywork = req.query.voluntarywork
+  };
+  if (req.query.pension) {
+    req.session.sectionStatus.pension = req.query.pension
+  };
+  if (req.query.insurance) {
+    req.session.sectionStatus.insurance = req.query.insurance
+  };
+  if (req.query.yourdetails) {
+    req.session.sectionStatus.yourdetails = req.query.yourdetails
+  };
+  if (req.query.submitted) {
+    req.session.sectionStatus.submitted = req.query.submitted
   };
 
   res.render('apply/v9/list.html', {sectionStatus: req.session.sectionStatus});
